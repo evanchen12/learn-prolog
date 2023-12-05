40eecb019ace0735d48766132d2fc7f9cd8758ec
evanchen12 <71461932+evanchen12@users.noreply.github.com>
2023-12-05 16:36:16 -0500

    Update cmember.pl

--- exercises/cmember.pl
+++ exercises/cmember.pl
@@ -2,6 +2,9 @@
 * Write a predicate cmember(X,L,N) which finds the first occurrence
 * of X in L if X > N; otherwise finds the first occurrence of N in L.
 */
+% Simulate a if else condition
+cmember(X, L, N) :- X > N, !, member1(X, L).
+cmember(X, L, N) :- member1(N, L).
 
 member1(X, [X | _]) :- !.
 member1(X, [_ | T]) :- member1(X,T).
